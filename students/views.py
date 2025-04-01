fbb00080b875410cc7e5f4a1786390e48babaa1c
its_sassinzz13 <66528388+sassinzz13@users.noreply.github.com>
2025-04-02 00:19:50 +0800

    Delete students directory

--- students/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Student
-# Create your views here.
-
-def student_list(request):
-    student_query = Student.objects.all()
-    return render(request, "student_data.html", {'student_query': student_query})
-
-
